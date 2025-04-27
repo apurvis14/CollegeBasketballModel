680827bdb7c44d44cb9e1960ff20d8b3b676806a
apurvis14 <purvis12@purdue.edu>
2025-04-27 19:57:28 -0400

    Width and height

--- CollegeBasketballModel.py
+++ CollegeBasketballModel.py
@@ -58,7 +58,7 @@ with st.container():
 
     with col2:
         st.markdown("<br> </br>", unsafe_allow_html=True)  # Empty
-        st.image(logo, width=750)
+        st.image(logo, width=500, height=500)
 
 
 
