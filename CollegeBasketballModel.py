3f2179ea43252a1d977e82a7bc2d7385fa3c3704
apurvis14 <purvis12@purdue.edu>
2025-06-08 15:45:18 -0500

    change top description

--- CollegeBasketballModel.py
+++ CollegeBasketballModel.py
@@ -75,7 +75,9 @@ st.markdown(
     <h1 style='text-align: center;'>College Basketball Trends Dashboard</h1>
     <p style='text-align: center;'>
     This dashboard explores win/loss for various trends across multiple calculations using offensive/defensive ratings, 
-    PPG, EFF, and tempo. Select a trend to see performance stats. <u>Scroll down to view games for today.<u>
+    PPG, EFF, and tempo. Select a trend to see performance stats. <u>Scroll down to view games for today.</u><br><br>
+    Over Net Units means how many units won by betting on the Over <br>
+    Under Net Units means how many units won by betting on the Under <br>
     </p>
     """,
     unsafe_allow_html=True
