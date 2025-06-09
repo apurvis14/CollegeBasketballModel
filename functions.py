7060fc11135826a565a6f9358437430f8397f54c
apurvis14 <purvis12@purdue.edu>
2025-06-08 20:23:21 -0500

    save

--- functions.py
+++ functions.py
@@ -554,7 +554,7 @@ def display_total_difference_histogram(plot_df):
         SECONDARY_COLOR = '#000000' # Black
         LINE_COLOR = '#FF5733'      # A pop color for vertical lines
         sns.histplot(plot_df['Total Difference'], bins=20, kde=False, ax=ax, color=PRIMARY_COLOR)
-        sns.kdeplot(plot_df['Total Difference'], ax=ax, color=SECONDARY_COLOR, linewidth=2, bw_adjust=1)
+        sns.kdeplot(plot_df['Total Difference'], ax=ax, color=SECONDARY_COLOR, line_width=2, bw_adjust=2)
         ax.axvline(x=0, color=LINE_COLOR, linestyle='--', label='Even Line')
         ax.set_title('Distribution of Difference from Book Total', fontsize=10)
         ax.set_xlabel('Total Difference (Actual - Book)', fontsize=9)
