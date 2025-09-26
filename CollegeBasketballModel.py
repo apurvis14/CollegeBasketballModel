7a01ae427909b9919b7ddbcfa78f9aaa940be6e6
apurvis14 <purvis12@purdue.edu>
2025-09-26 00:09:59 -0500

    Revert Back

--- CollegeBasketballModel.py
+++ CollegeBasketballModel.py
@@ -295,14 +295,13 @@ with tab9:
                 full_html = f"""
                 <style>
                 details {{
-                    background-color:#000000;
-                    color:#ffffff;
+                    background-color:#ffffff;
+                    color:#000;
                     border-radius:8px;
                     padding:0;
                     margin:0;
                 }}
                 summary {{
-                    background-color:#ffffff;
                     list-style:none;
                     cursor:pointer;
                     display:flex;
