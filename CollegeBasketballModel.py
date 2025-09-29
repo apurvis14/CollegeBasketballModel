8372e137c033f29d842775705b701054e67ad1e0
apurvis14 <purvis12@purdue.edu>
2025-09-29 13:04:44 -0500

    Margins

--- CollegeBasketballModel.py
+++ CollegeBasketballModel.py
@@ -274,7 +274,9 @@ with tab9:
 
                 # Adjust height to fit (or compute dynamically). Use scrolling if content is larger.
                 height = int(estimate_height(trend_html))
+                st.markdown("<div style='margin-bottom:6px;'>", unsafe_allow_html=True)
                 st_html(details_html(matchup_header, trend_html), height=height, scrolling=False)
+                st.markdown("</div>", unsafe_allow_html=True)
             
             elif game['Efficiency/PPG over  (Tempo under)'] == 1:
                 trend_html = show_trend_html(
