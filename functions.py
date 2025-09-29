3e16023091d835feb1decc83c1283e3f4dcefb41
apurvis14 <purvis12@purdue.edu>
2025-09-29 17:14:24 -0500

    Remove Margin Bottom Title

--- functions.py
+++ functions.py
@@ -1070,7 +1070,7 @@ def show_trend_html(
 
     html1 = f"""
     <div style="border:2px solid #DAA520; border-radius:8px; padding:8px; background-color:#000000; margin-bottom:2px;">
-        <div style="display:flex; justify-content:space-around; font-size:26px; margin-bottom:4px">
+        <div style="display:flex; justify-content:space-around; font-size:26px; margin-bottom:0px">
             <div style="text-align:center;">
                 <h4 style="font-style:italic; margin:0;">System Active: {title}</h4><br>
             </div>
