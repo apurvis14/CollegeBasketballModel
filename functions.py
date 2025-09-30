33777d5d5aad48bfba1399a08a737292e583a5ce
apurvis14 <purvis12@purdue.edu>
2025-09-30 15:05:04 -0500

    Remove Flex-Direction

--- functions.py
+++ functions.py
@@ -1171,7 +1171,6 @@ def details_html(matchup_header, trend_html):
         list-style: none;
         cursor: pointer;
         display: flex;
-        flex-direction: column;  /* ✅ arrow + content side by side */
         align-items: left;
         padding: 0.75rem 1rem;
     }}
