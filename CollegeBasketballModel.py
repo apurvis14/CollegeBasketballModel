66e5fb70a3f3de79861e987d60ab8c77ea89e5e8
apurvis14 <purvis12@purdue.edu>
2025-09-30 20:32:33 -0500

    Add Necessary Stuff

--- CollegeBasketballModel.py
+++ CollegeBasketballModel.py
@@ -38,7 +38,8 @@ st.markdown(
 
 ENCODED_USERS = {
     "U2FtIERyZW5uYW46MTIzNDU=": "Sam Drennan",
-    "QW5kcmV3IEdyaWZmaW46NTY3ODk=": "Andrew Griffin"
+    "QW5kcmV3IEdyaWZmaW46NTY3ODk=": "Andrew Griffin",
+    "SGFtcCBIdWRuYWxsOjEzNTc5=": "Hamp Hudnall"
 }
 
 # ---------- SESSION STATE INIT ----------
