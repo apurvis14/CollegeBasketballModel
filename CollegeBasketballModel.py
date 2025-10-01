20330518ea4eeae0998a4d872afb97e4d78183ff
apurvis14 <purvis12@purdue.edu>
2025-09-30 19:21:27 -0500

    Today Date

--- CollegeBasketballModel.py
+++ CollegeBasketballModel.py
@@ -175,7 +175,7 @@ with tab9:
         )
 
     # Today Variable for Sorting
-    today = datetime(2025, 2, 27).date()
+    today = datetime(2025, 2, 15).date()
 
     # Names of Columns
     home_col = 'Home Conference'
