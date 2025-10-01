8a82f8530a4f2b746bb3d064ffc217850cc1481e
apurvis14 <purvis12@purdue.edu>
2025-09-30 19:20:13 -0500

    try this look

--- functions.py
+++ functions.py
@@ -844,9 +844,9 @@ def display_metrics_expand(percent):
     elif 48 < percent < 52:
         percent_display = f"<span style='color:black; background-color:white; font-weight:bold'>({percent}%)</span>"
     elif 46 < percent <=48:
-        percent_display = f"<span style=color:black; background-'color:orange; font-weight:bold'>({percent}%)</span>"
+        percent_display = f"<span style=color:black; background-'color:gold; font-weight:bold'>({percent}%)</span>"
     elif 40 <= percent <= 46:
-        percent_display = f"<span style='color:black; background-color:orangered; font-weight:bold'>({percent}%)</span>"
+        percent_display = f"<span style='color:black; background-color:#FF6933; font-weight:bold'>({percent}%)</span>"
     elif percent < 40:
         percent_display = f"<span style='color:black; background-color:red; font-weight:bold'>({percent}%)</span>"
     else:
