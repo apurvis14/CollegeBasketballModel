ba00050c7da59ecd202e3c3af7390628191899ec
apurvis14 <purvis12@purdue.edu>
2025-12-23 20:03:52 -0700

    Try this

--- CollegeBasketballModel.py
+++ CollegeBasketballModel.py
@@ -541,7 +541,7 @@ with tab1:
         home_img = f'<img class="team-logo" src="data:image/jpeg;base64,{home_logo}">'
 
         matchup_header = (
-            f"{away_img} {game['Away Team']} @ &nbsp;{home_img} {game['Home Team']} "
+            f"{away_img} {game['Away Team']} @ &nbsp;{home_img} {game['Home Team']} {need} Game"
             f"&nbsp;|&nbsp; Total: {game['Book Total']}||"
             f"'25-'26 Trend Over Record: {record_cur} {percent_cur1} <br>"
             f"All Time Trend Over Record: {record_all} {percent_all1} <br>"
